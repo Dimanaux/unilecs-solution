cf91047b1b23e87e48b7588c269fbfc3e74056b5
Dmitry Barskov <barskoidim@gmail.com>
2018-09-24 21:06:58 +0300

    add assertFalse method

--- 0001/Test.java
+++ 0001/Test.java
@@ -9,15 +9,19 @@ public class Test {
     }
 
     public static void test(Solution solution) {
-        assertTrue(!solution.allUnique("hello world"));
-        assertTrue(!solution.allUnique("some string"));
-        assertTrue(!solution.allUnique("two spaces :)"));
-        assertTrue(!solution.allUnique("abcdefg ah"));
-        
+        assertFalse(solution.allUnique("hello world"));
+        assertFalse(solution.allUnique("some string"));
+        assertFalse(solution.allUnique("two spaces :)"));
+        assertFalse(solution.allUnique("abcdefg ah"));
+
         assertTrue(solution.allUnique("0123456789"));
         assertTrue(solution.allUnique("uniq chars!"));
         assertTrue(solution.allUnique("qwertyuiop"));
         assertTrue(solution.allUnique("QqWeRrTtYy"));
+    }
+
+    public static void assertFalse(boolean expression) {
+        assertTrue(!expression);
     }
 
     public static void assertTrue(boolean expression) {
